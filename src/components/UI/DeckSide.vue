00ad45cea804ab022b011a49bff67dd3c9141b36
irnq <47535562+irnq@users.noreply.github.com>
2022-02-14 23:44:37 +0400

    refactor: del unused Watch

--- src/components/UI/DeckSide.vue
+++ src/components/UI/DeckSide.vue
@@ -41,7 +41,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 import { mapGetters } from 'vuex';
 import userInterface from '../interface/user';
 import ModalWindow from '../ModalWindow/ModalWindow.vue';
