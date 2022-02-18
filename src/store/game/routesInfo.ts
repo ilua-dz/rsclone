65d2a58b91a065c4881d70458b082271fde14fdc
Ilya Dzyuin <ilua.dzuin@gmail.com>
2022-02-18 16:40:33 +0400

    feat: add routes game information

---
+++ src/store/game/routesInfo.ts
@@ -0,0 +1,234 @@
+const routesInfo = [
+  {
+    cities: ['Athina', 'Angora'],
+    id: 0,
+    points: 5,
+  },
+  {
+    cities: ['Budapest', 'Sofia'],
+    id: 1,
+    points: 5,
+  },
+  {
+    cities: ['Frankfurt', 'Kobenhavn'],
+    id: 2,
+    points: 5,
+  },
+  {
+    cities: ['Rostov', 'Erzurum'],
+    id: 3,
+    points: 5,
+  },
+  {
+    cities: ['Sofia', 'Smyrna'],
+    id: 4,
+    points: 5,
+  },
+  {
+    cities: ['Kyiv', 'Petrograd'],
+    id: 5,
+    points: 6,
+  },
+  {
+    cities: ['Zurich', 'Brindisi'],
+    id: 6,
+    points: 6,
+  },
+  {
+    cities: ['Zurich', 'Budapest'],
+    id: 7,
+    points: 6,
+  },
+  {
+    cities: ['Warszawa', 'Smolensk'],
+    id: 8,
+    points: 6,
+  },
+  {
+    cities: ['Zagrab', 'Brindisi'],
+    id: 9,
+    points: 6,
+  },
+  {
+    cities: ['Paris', 'Zagrab'],
+    id: 10,
+    points: 7,
+  },
+  {
+    cities: ['Brest', 'Marseille'],
+    id: 11,
+    points: 7,
+  },
+  {
+    cities: ['London', 'Berlin'],
+    id: 12,
+    points: 7,
+  },
+  {
+    cities: ['Edinburgh', 'Paris'],
+    id: 13,
+    points: 7,
+  },
+  {
+    cities: ['Amsterdam', 'Pamplona'],
+    id: 14,
+    points: 7,
+  },
+  {
+    cities: ['Roma', 'Smyrna'],
+    id: 15,
+    points: 8,
+  },
+  {
+    cities: ['Palermo', 'Constantinople'],
+    id: 16,
+    points: 8,
+  },
+  {
+    cities: ['Sarajevo', 'Sevastopol'],
+    id: 17,
+    points: 8,
+  },
+  {
+    cities: ['Madrid', 'Dieppe'],
+    id: 18,
+    points: 8,
+  },
+  {
+    cities: ['Barcelona', 'Bruxelles'],
+    id: 19,
+    points: 8,
+  },
+  {
+    cities: ['Paris', 'Wien'],
+    id: 20,
+    points: 8,
+  },
+  {
+    cities: ['Barcelona', 'Munchen'],
+    id: 21,
+    points: 8,
+  },
+  {
+    cities: ['Brest', 'Venezia'],
+    id: 22,
+    points: 8,
+  },
+  {
+    cities: ['Smolensk', 'Rostov'],
+    id: 23,
+    points: 8,
+  },
+  {
+    cities: ['Marseille', 'Essen'],
+    id: 24,
+    points: 8,
+  },
+  {
+    cities: ['Kyiv', 'Sochi'],
+    id: 25,
+    points: 8,
+  },
+  {
+    cities: ['Madrid', 'Zurich'],
+    id: 26,
+    points: 8,
+  },
+  {
+    cities: ['Berlin', 'Bucuresti'],
+    id: 27,
+    points: 8,
+  },
+  {
+    cities: ['Bruxelles', 'Danzig'],
+    id: 28,
+    points: 9,
+  },
+  {
+    cities: ['Berlin', 'Roma'],
+    id: 29,
+    points: 9,
+  },
+  {
+    cities: ['Angora', 'Kharkov'],
+    id: 30,
+    points: 10,
+  },
+  {
+    cities: ['Riga', 'Bucuresti'],
+    id: 31,
+    points: 10,
+  },
+  {
+    cities: ['Essen', 'Kyiv'],
+    id: 32,
+    points: 10,
+  },
+  {
+    cities: ['Venizia', 'Constantinople'],
+    id: 33,
+    points: 10,
+  },
+  {
+    cities: ['London', 'Wien'],
+    id: 34,
+    points: 10,
+  },
+  {
+    cities: ['Athina', 'Wilno'],
+    id: 35,
+    points: 11,
+  },
+  {
+    cities: ['Stockholm', 'Wien'],
+    id: 36,
+    points: 11,
+  },
+  {
+    cities: ['Berlin', 'Moskva'],
+    id: 37,
+    points: 12,
+  },
+  {
+    cities: ['Amsterdam', 'Wilno'],
+    id: 38,
+    points: 12,
+  },
+  {
+    cities: ['Frankfurt', 'Smolensk'],
+    id: 39,
+    points: 13,
+  },
+  {
+    cities: ['Lisboa', 'Danzig'],
+    id: 40,
+    points: 20,
+  },
+  {
+    cities: ['Brest', 'Petrograd'],
+    id: 41,
+    points: 20,
+  },
+  {
+    cities: ['Palermo', 'Moskva'],
+    id: 42,
+    points: 20,
+  },
+  {
+    cities: ['Kobenhavn', 'Erzurum'],
+    id: 43,
+    points: 21,
+  },
+  {
+    cities: ['Edinburgh', 'Athina'],
+    id: 44,
+    points: 21,
+  },
+  {
+    cities: ['Cadiz', 'Stockholm'],
+    id: 45,
+    points: 21,
+  },
+];
+
+export default routesInfo;
